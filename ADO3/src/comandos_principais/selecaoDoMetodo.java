985f98d69606b576d27cf61b17726e49955f14b1
Yasmim Nascimento <yasmim.ca19@gmail.com>
2025-10-27 20:09:25 -0300

    Update selecaoDoMetodo.java

--- ADO3/src/comandos_principais/selecaoDoMetodo.java
+++ ADO3/src/comandos_principais/selecaoDoMetodo.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package comandos_principais;
 import basesMetodosOrdenacao.OrdenacaoBase;
 import metodosDeOrdenacao.Selecao;
@@ -9,10 +5,7 @@ import metodosDeOrdenacao.Insercao;
 import metodosDeOrdenacao.Bolha;
 import ado3.*;
 
-/**
- *
- * @author yasmi
- */
+
 public class selecaoDoMetodo {
     static OrdenacaoBase metodo;
     public static OrdenacaoBase escolha(int op_escolhida){
@@ -36,3 +29,4 @@ public class selecaoDoMetodo {
     }
     
 }
+
